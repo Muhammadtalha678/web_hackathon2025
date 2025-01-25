2384742677c85402045883623576a179119d94c8
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-01-25 15:31:08 +0500

    remove suspense inport

--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState, Suspense } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
